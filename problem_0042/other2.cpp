cd3bcf38d09f1beae1d9036b062f27e785cb7bcb
Le <nguyenle@amazon.com>
2018-04-21 21:25:54 +0200

    Add another solution

---
+++ problem_0042/other2.cpp
@@ -0,0 +1,14 @@
+/*
+Keep track of the already safe level and the total water so far. In each step, process and discard the lower one of the leftmost or rightmost elevation.
+*/
+
+int trap(vector<int>& height) {
+    int l = 0, r = height.size()-1, level = 0, water = 0;
+    while (l < r) {
+        int lower = height[height[l] < height[r] ? l++ : r--];
+        level = max(level, lower);
+        water += level - lower;
+    }
+    return water;
+}
+
