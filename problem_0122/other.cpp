11cd0a44def4c3d7c23923e06326277d287cfdde
Le <nguyenle@amazon.com>
2018-05-06 22:41:43 +0200

    Add another solution

---
+++ problem_0122/other.cpp
@@ -0,0 +1,11 @@
+/*
+Suppose the first sequence is "a <= b <= c <= d", the profit is "d - a = (b - a) + (c - b) + (d - c)" without a doubt. And suppose another one is "a <= b >= b' <= c <= d", the profit is not difficult to be figured out as "(b - a) + (d - b')". So you just target at monotone sequences.
+*/
+
+int maxProfit(vector<int> &prices) {
+    int ret = 0;
+    for (size_t p = 1; p < prices.size(); ++p) 
+      ret += max(prices[p] - prices[p - 1], 0);    
+    return ret;
+}
+
