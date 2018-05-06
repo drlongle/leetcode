7d504f5a7dc41ca8b35743d9895224b6ce643338
Le <nguyenle@amazon.com>
2018-05-06 21:46:24 +0200

    Add another solution

---
+++ problem_0116/Other.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null) return;
+        if(root.left == null && root.right == null)  return;
+        if(root.left != null && root.right != null)
+            root.left.next = root.right;
+        if(root.next != null)
+            root.right.next = root.next.left;
+        
+        connect(root.left);
+        connect(root.right);
+    }
+    
+}
+
