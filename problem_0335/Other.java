f301183a9d0dbff6e7f99e8296e9825211321b11
Long Le <drlongle@gmail.com>
2018-08-03 18:54:53 +0200

    Add solution for problem 335 Self Crossing

---
+++ problem_0335/Other.java
@@ -0,0 +1,27 @@
+// Categorize the self-crossing scenarios, there are 3 of them: 
+// 1. Fourth line crosses first line and works for fifth line crosses second line and so on...
+// 2. Fifth line meets first line and works for the lines after
+// 3. Sixth line crosses first line and works for the lines after
+public class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        int l = x.length;
+        if(l <= 3) return false;
+        
+        for(int i = 3; i < l; i++){
+	    //Fourth line crosses first line and onward
+            if(x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;
+            if(i >=4)
+            {
+		// Fifth line meets first line and onward
+                if(x[i-1] == x[i-3] && x[i] + x[i-4] >= x[i-2]) return true;
+            }
+            if(i >=5)
+            {
+		// Sixth line crosses first line and onward
+                if(x[i-2] - x[i-4] >= 0 && x[i] >= x[i-2] - x[i-4] && x[i-1] >= x[i-3] - x[i-5] && x[i-1] <= x[i-3]) return true;
+            }
+        }
+        return false;
+    }
+}
+
