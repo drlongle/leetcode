0b7868502a7cae888bc1376790bf4762cea66e2b
Long Le <drlongle@gmail.com>
2018-11-26 22:50:23 +0000

    Minor changes for solution 352 (Data Stream as Disjoint Intervals)

--- problem_0352/solution.cpp
+++ problem_0352/solution.cpp
@@ -68,8 +68,7 @@ struct Node {
     }
 
     void merge() {
-        auto it = children.begin();
-        int i = 0;
+        size_t i = 0;
         while (i < children.size() -1) {
             auto& curr = children[i];
             auto& next = children[i+1];
@@ -148,7 +147,8 @@ struct Node {
 class SummaryRanges {
 public:
     /** Initialize your data structure here. */
-    SummaryRanges() : root(Node(numeric_limits<int>::max(), numeric_limits<int>::min(), true)) {
+    SummaryRanges() : root(Node(numeric_limits<int>::max(), numeric_limits<int>::min(), true))
+    {
 
     }
 
@@ -181,24 +181,10 @@ void print_vector(const vector<Interval>& intervals)
 void test_case_1()
 {
     SummaryRanges sr;
-    cout << "Execuging test case 1" << endl;
-    
     sr.addNum(1);
-    print_vector(sr.getIntervals());
-    cout << "----" << endl;
-
     sr.addNum(3);
-    print_vector(sr.getIntervals());
-    cout << "----" << endl;
-
     sr.addNum(7);
-    print_vector(sr.getIntervals());
-    cout << "----" << endl;
-
     sr.addNum(2);
-    print_vector(sr.getIntervals());
-    cout << "----" << endl;
-
     sr.addNum(6);
     print_vector(sr.getIntervals());
 }
@@ -226,11 +212,14 @@ void test_case_3()
 
 void test_case_4()
 {
-    vector<int> nums{49,97,53,5,33,65,62,51,100,38,61,45,74,27,64,17,36,17,96,12,79,32,68,90,77,18,39,12,93,9,87,42,60,71,12,45,55,40,78,81,26,70,61,56,66,33,7,70,1,11,92,51,90,100,85,80,0,78,63,42,31,93,41,90,8,24,72,28,30,18,69,57,11,10,40,65,62,13,38,70,37,90,15,70,42,69,26,77,70,75,36,56,11,76,49,40,73,30,37,23,};
+    vector<int> nums{49,97,53,5,33,65,62,51,100,38,61,45,74,27,64,17,36,17,96,12,79,32,68,90,
+    77,18,39,12,93,9,87,42,60,71,12,45,55,40,78,81,26,70,61,56,66,33,7,70,1,11,92,51,90,100,
+    85,80,0,78,63,42,31,93,41,90,8,24,72,28,30,18,69,57,11,10,40,65,62,13,38,70,37,90,15,70,
+    42,69,26,77,70,75,36,56,11,76,49,40,73,30,37,23};
+
     SummaryRanges sr;
     for (auto& n: nums) {
         sr.addNum(n);
-        cout << "--------------- adding " << n << endl;
         print_vector(sr.getIntervals());
     }
     print_vector(sr.getIntervals());
@@ -238,7 +227,7 @@ void test_case_4()
 
 int main(int argc, char** argv) {
 
-    test_case_5();
+    test_case_4();
 
     return 0;
 }
