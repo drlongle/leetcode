c6e99fdc7335ef32cd3cc40ddd85fbd0a20dcac9
Long Le <drlongle@gmail.com>
2019-08-22 22:09:23 +0100

    Add solution for problem 417

--- problem_0417/solution.cpp
+++ problem_0417/solution.cpp
@@ -64,15 +64,76 @@ using namespace std;
 
 class Solution {
 public:
-    vector<pair<int, int>> pacificAtlantic(vector<vector<int>>& matrix) {
 
+    int rows;
+    int columns;
+    queue<pair<int, int>> q;
+    vector<vector<int>> visited, res;
+    
+    void bfs(const vector<vector<int>>& matrix, int mask) {
+        const array<array<int, 2>, 4> offsets{1, 0, -1, 0, 0, 1, 0, -1};
+        while (!q.empty()) {
+            auto& c = q.front();
+            int x = c.first;
+            int y = c.second;
+            if (visited[x][y] != 3 && (visited[x][y] | mask) == 3)
+                res.emplace_back(vector<int>{x, y});
+            visited[x][y] |= mask;
+
+            for (auto& off: offsets) {
+                int new_x = x + off[0];
+                int new_y = y + off[1];
+                if (new_x >= 0 && new_x < rows && new_y >= 0 && new_y < columns
+                    && matrix[x][y] <= matrix[new_x][new_y] && (visited[new_x][new_y] & mask) == 0)
+                    q.emplace(new_x, new_y);
+            }
+            q.pop();
+        }
+    }
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) {
+        rows = matrix.size();
+        if (rows == 0)
+            return res;
+        columns = matrix[0].size();
+        if (columns == 0)
+            return res;
+        
+        visited.resize(rows, vector<int>(columns, 0));
+        for (int j = 0; j < columns; ++j)
+            q.emplace(0, j);
+        for (int i = 0; i < rows; ++i)
+            q.emplace(i, 0);
+        bfs(matrix, 1);
+
+        for (int j = 0; j < columns; ++j)
+            q.emplace(rows-1, j);
+        for (int i = 0; i < rows; ++i)
+            q.emplace(i, columns-1);
+        bfs(matrix, 2);
+        return res;
     }
 };
 
 int main()
 {
     Solution sol;
+    vector<vector<int>> matrix;
 
+    matrix = {
+        {1,2,2,3,5},
+        {3,2,3,4,4},
+        {2,4,5,3,1},
+        {6,7,1,4,5},
+        {5,1,1,2,4}
+    };
+    auto res = sol.pacificAtlantic(matrix);
+
+    for (auto& p: res) {
+        copy(begin(p), end(p), ostream_iterator<int>(cout, ", "));
+        cout << endl;
+    }
+    
     return 0;
 }
 
