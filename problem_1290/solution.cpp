e539dd051358344638d154cfaac80b47c2f7e203
Long Le <drlongle@gmail.com>
2019-12-17 21:52:53 +0000

    Add problem 1290

---
+++ problem_1290/solution.cpp
@@ -0,0 +1,85 @@
+/*
+1290. Convert Binary Number in a Linked List to Integer
+Easy
+
+Given head which is a reference node to a singly-linked list. The value of each node in the linked
+list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+Example 1:
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+Input: head = [0]
+Output: 0
+
+Example 3:
+Input: head = [1]
+Output: 1
+
+Example 4:
+Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+Output: 18880
+
+Example 5:
+Input: head = [0,0]
+Output: 0
+
+Constraints:
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int sum = 0;
+        while (head) {
+            sum = sum*2 + head->val;
+            head = head->next;
+        }
+        return sum;
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
