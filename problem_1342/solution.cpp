71de1a97286f5f794d82e4402ecf942fb9e997ca
Long Le <drlongle@gmail.com>
2020-02-10 22:34:30 +0000

    Add problem 1342

---
+++ problem_1342/solution.cpp
@@ -0,0 +1,74 @@
+/*
+1342. Number of Steps to Reduce a Number to Zero
+Easy
+
+Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it. 
+
+Example 1:
+Input: num = 14
+Output: 6
+Explanation: 
+Step 1) 14 is even; divide by 2 and obtain 7. 
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3. 
+Step 4) 3 is odd; subtract 1 and obtain 2. 
+Step 5) 2 is even; divide by 2 and obtain 1. 
+Step 6) 1 is odd; subtract 1 and obtain 0.
+
+Example 2:
+Input: num = 8
+Output: 4
+Explanation: 
+Step 1) 8 is even; divide by 2 and obtain 4. 
+Step 2) 4 is even; divide by 2 and obtain 2. 
+Step 3) 2 is even; divide by 2 and obtain 1. 
+Step 4) 1 is odd; subtract 1 and obtain 0.
+Example 3:
+
+Input: num = 123
+Output: 12
+
+Constraints:
+0 <= num <= 10^6
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+class Solution {
+public:
+    int numberOfSteps (int num) {
+        if (!num) return 0;
+        if (num & 1) return 1 + numberOfSteps(num-1);
+        else return 1 + numberOfSteps(num / 2);
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
