673d8124cb4cdcdced771e45e7380d42b7f4b583
Long Le <drlongle@gmail.com>
2020-02-29 22:11:29 +0000

    Improve solution for problem 0933

---
+++ problem_0933/solution.cpp
@@ -0,0 +1,76 @@
+/*
+933. Number of Recent Calls
+Easy
+
+Write a class RecentCounter to count recent requests.
+
+It has only one method: ping(int t), where t represents some time in milliseconds.
+
+Return the number of pings that have been made from 3000 milliseconds ago until now.
+
+Any ping with time in [t - 3000, t] will count, including the current ping.
+
+It is guaranteed that every call to ping uses a strictly larger value of t than before.
+
+Example 1:
+
+Input: inputs = ["RecentCounter","ping","ping","ping","ping"], inputs = [[],[1],[100],[3001],[3002]]
+Output: [null,1,2,3,3]
+
+Note:
+
+Each test case will have at most 10000 calls to ping.
+Each test case will call ping with strictly increasing values of t.
+Each call to ping will have 1 <= t <= 10^9.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+class RecentCounter {
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        ts.push(t);
+        int thres = t - 3000;
+        while (true) {
+            if (ts.front() < thres)
+                ts.pop();
+            else
+                break;
+        }
+        return ts.size();
+    }
+    queue<int> ts;
+};
+
+int main() {
+    return 0;
+}
