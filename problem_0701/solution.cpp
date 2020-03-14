eee7630d283428af7e7669f518a7def2cb708a60
Long Le <drlongle@gmail.com>
2020-03-14 20:55:46 +0000

    Add problem 701

---
+++ problem_0701/solution.cpp
@@ -0,0 +1,92 @@
+/*
+701. Insert into a Binary Search Tree
+Medium
+
+Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+And the value to insert: 5
+You can return this binary search tree:
+
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+This tree is also valid:
+         5
+       /   \
+      2     7
+     / \   
+    1   3
+         \
+          4
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (val < root->val) {
+            if (root->left)
+                insertIntoBST(root->left, val);
+            else
+                root->left = new TreeNode(val);
+        } else {
+            if (root->right)
+                insertIntoBST(root->right, val);
+            else
+                root->right = new TreeNode(val);
+        }
+
+        return root;
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
