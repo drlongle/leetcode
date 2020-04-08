5c1f019bf9caad94cc62f6b3bf4b4570c8bc42fd
Long Le <drlongle@gmail.com>
2020-04-08 09:33:41 +0200

    Add problem 876

---
+++ problem_0876/solution.cpp
@@ -0,0 +1,75 @@
+/*
+876. Middle of the Linked List
+Easy
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+
+Example 2:
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+Note:
+The number of nodes in the given list will be between 1 and 100.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        auto fast = head, slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
+
+int main() {
+    return 0;
+}
