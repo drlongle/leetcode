b299d3b6e00388e0b9c3efddc695672d5f9e63ad
Long Le <drlongle@gmail.com>
2020-04-11 17:53:39 +0200

    Add problem 543

---
+++ problem_0543/solution.cpp
@@ -0,0 +1,83 @@
+/*
+543. Diameter of Binary Tree
+Easy
+
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+Example:
+Given a binary tree
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int res;
+
+    int get_height(TreeNode* node) {
+        if (node == nullptr)
+            return 0;
+
+        int hl = get_height(node->left);
+        int hr = get_height(node->right);
+        res = max(res, hl + hr);
+
+        return 1 + max(hr, hl);;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        res = 0;
+        get_height(root);
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
