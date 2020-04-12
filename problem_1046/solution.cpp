f3fcca511431e02f8dd4c06f6745ca7a61e368cd
Long Le <drlongle@gmail.com>
2020-04-12 09:39:22 +0200

    Add problem 1046

---
+++ problem_1046/solution.cpp
@@ -0,0 +1,85 @@
+/*
+1046. Last Stone Weight
+Easy
+
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+Example 1:
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+Note:
+1 <= stones.length <= 30
+1 <= stones[i] <= 1000
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int, vector<int>> pq(begin(stones), end(stones));
+        while (!pq.empty()) {
+            int res = pq.top();
+            pq.pop();
+            if (pq.empty())
+                return res;
+            res -= pq.top();
+            pq.pop();
+            if (res)
+                pq.emplace(res);
+        }
+
+        return 0;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> stones;
+
+    stones = {2,7,4,1,8,1};
+    cout << sol.lastStoneWeight(stones) << endl;
+    
+    return 0;
+}
