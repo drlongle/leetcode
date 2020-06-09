17acd1e96a3eb7bed2a6b9dd45f25eea1bb61ebc
Long Le <drlongle@gmail.com>
2020-06-09 22:19:37 +0200

    Add solution for problem 951

---
+++ problem_0951/solution.cpp
@@ -0,0 +1,75 @@
+/*
+951. Flip Equivalent Binary Trees
+Medium
+
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Write a function that determines whether two binary trees are flip equivalent.  The trees are given by root nodes root1 and root2.
+
+Example 1:
+Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+Output: true
+Explanation: We flipped at nodes with values 1, 3, and 5.
+
+Note:
+Each tree will have at most 100 nodes.
+Each value in each tree will be a unique integer in the range [0, 99].
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2)
+            return true;
+        if (!root1 || !root2)
+            return false;
+        if (root1->val != root2->val)
+            return false;
+        return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right))
+            || (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
