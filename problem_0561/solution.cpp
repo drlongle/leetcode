b96f0884c3455f83bc6efc3eeefe0e782cb4a7e7
Long Le <drlongle@gmail.com>
2020-07-22 00:26:49 +0200

    Add solution for problem 561

---
+++ problem_0561/solution.cpp
@@ -0,0 +1,60 @@
+/*
+561. Array Partition I
+Easy
+
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say
+(a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example 1:
+Input: [1,4,3,2]
+Output: 4
+
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+Note:
+n is a positive integer, which is in the range of [1, 10000].
+All the integers in the array will be in the range of [-10000, 10000].
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int res = 0, sz = nums.size();
+        for (int i = 0; i < sz; i+= 2)
+            res += nums[i];
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
