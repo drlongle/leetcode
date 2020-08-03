38b9d2efa3d7f45856fff63ac10dd4dcf8e8a2bc
Long Le <drlongle@gmail.com>
2020-08-03 09:15:25 +0200

    Add solution for problem 520

---
+++ problem_0520/solution.cpp
@@ -0,0 +1,66 @@
+/*
+520. Detect Capital
+Easy
+
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+Example 1:
+Input: "USA"
+Output: True
+
+Example 2:
+Input: "FlaG"
+Output: False
+
+Note: The input will be a non-empty word consisting of uppercase and
+lowercase latin letters.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int sz = word.size();
+        int capitals = count_if(word.begin(), word.end(), [] (char ch) {return isupper(ch); });
+        return capitals == 0 || capitals == sz || (capitals == 1 && isupper(word[0]));
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
