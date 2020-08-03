39f8effd14543f8b077bbfa921c23c32f20be69a
Long Le <drlongle@gmail.com>
2020-08-03 09:29:27 +0200

    Add solution for problem 706

---
+++ problem_0706/solution.cpp
@@ -0,0 +1,93 @@
+/*
+706. Design HashMap
+Easy
+
+Design a HashMap without using any built-in hash table libraries.
+
+To be specific, your design should include these functions:
+
+put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
+get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
+
+Example:
+
+MyHashMap hashMap = new MyHashMap();
+hashMap.put(1, 1);          
+hashMap.put(2, 2);         
+hashMap.get(1);            // returns 1
+hashMap.get(3);            // returns -1 (not found)
+hashMap.put(2, 1);          // update the existing value
+hashMap.get(2);            // returns 1 
+hashMap.remove(2);          // remove the mapping for 2
+hashMap.get(2);            // returns -1 (not found) 
+
+Note:
+
+All keys and values will be in the range of [0, 1000000].
+The number of operations will be in the range of [1, 10000].
+Please do not use the built-in HashMap library.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        vals.resize(max_sz);
+        valid.resize(max_sz, false);
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        valid[key] = true;
+        vals[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        if (valid[key])
+            return vals[key];
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        valid[key] = false;
+    }
+    
+    static constexpr int max_sz = 1000001;
+    vector<int> vals;
+    vector<bool> valid;
+};
+
+int main() {
+    return 0;
+}
