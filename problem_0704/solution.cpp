215e2731a2cf396665c50375bd624953c0e2bbe8
Long Le <drlongle@gmail.com>
2020-10-08 10:44:25 +0200

    Add solution for problem 704

---
+++ problem_0704/solution.cpp
@@ -0,0 +1,75 @@
+/*
+704. Binary Search
+Easy
+
+Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+Note:
+You may assume that all elements in nums are unique.
+n will be in the range [1, 10000].
+The value of each element in nums will be in the range [-9999, 9999].
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size();
+        while (low <= high) {
+            int mid = (low+high)/2;
+            int diff = nums[mid] - target;
+            if (diff == 0)
+                return mid;
+            if (diff < 0)
+                low = mid + 1;
+            else
+                high = mid -1;
+        }
+        
+        return -1;      
+    }
+};
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
