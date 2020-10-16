cc7c2a456ade57923f61fdf635328f8ef4795c80
Long Le <drlongle@gmail.com>
2020-10-16 10:40:19 +0200

    Add additional solutions for problem 74

--- problem_0074/other1.cpp
+++ problem_0074/other1.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
         int n = matrix.size();
+        if (!n)
+            return false;
         int m = matrix[0].size();
+        if (!m)
+            return false;
         int l = 0, r = m * n - 1;
         while (l != r){
             int mid = (l + r - 1) >> 1;
@@ -14,4 +18,3 @@ public:
         return matrix[r / m][r % m] == target;
     }
 };
-
