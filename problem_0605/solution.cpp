d09cb8323c6e73c45eb3ce883d713554d89312e8
Long Le <drlongle@gmail.com>
2020-12-09 11:23:39 +0100

    Add solution for problem 605

---
+++ problem_0605/solution.cpp
@@ -0,0 +1,77 @@
+/*
+605. Can Place Flowers
+Easy
+
+You have a long flowerbed in which some of the plots are planted, and some are not.
+However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+and an integer n, return if n new flowers can be planted in the flowerbed without violating the
+no-adjacent-flowers rule.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+Constraints:
+1 <= flowerbed.length <= 2 * 10^4
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/TreeNode.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int sz = flowerbed.size();
+        for (int i = 0; i < sz && n > 0; ++i) {
+            if (!flowerbed[i] &&  (i == 0 || !flowerbed[i-1]) && (i == sz - 1 || !flowerbed[i+1])) {
+                --n;
+                flowerbed[i] = 1;
+            }
+        }
+
+        return n == 0;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
