1c28041ddb5d25b88d7b861568e8aa2428ed94fd
Long Le <drlongle@gmail.com>
2020-12-15 12:25:43 +0100

    Add an additional solution for problem 977

---
+++ problem_0977/solution2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res;
+        int i = 0, j = nums.size() - 1;
+        while (i <= j) {
+            int x = nums[i]*nums[i];
+            int y = nums[j]*nums[j];
+            if (x >= y) {
+                res.push_back(x);
+                ++i;
+            } else {
+                res.push_back(y);
+                --j;
+            }
+        }
+        reverse(res.begin(), res.end());
+        
+        return res;
+    }
+};
