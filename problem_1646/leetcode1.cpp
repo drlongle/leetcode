d6d2c195a578a2081ecc03e127b6d38074ba65e1
Long Le <drlongle@gmail.com>
2021-01-15 11:09:35 +0100

    Add solution for problem 1646

---
+++ problem_1646/leetcode1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int nums[n + 1];
+        int maximumValue = 1;
+        nums[1] = 1;
+        for (int i = 1; i <= n / 2; i++) {
+            nums[i * 2] = nums[i];
+            if (i * 2 + 1 <= n) {
+                nums[i * 2 + 1] = nums[i] + nums[i + 1];
+                maximumValue = max(maximumValue, nums[i * 2 + 1]);
+            }
+        }
+        return maximumValue;
+    }
+};
---
+++ problem_1646/solution.cpp
@@ -0,0 +1,92 @@
+/*
+1646. Get Maximum in Generated Array
+Easy
+
+You are given an integer n. An array nums of length n + 1 is generated in the following way:
+nums[0] = 0
+nums[1] = 1
+nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+Return the maximum integer in the array nums.
+
+Example 1:
+Input: n = 7
+Output: 3
+Explanation: According to the given rules:
+  nums[0] = 0
+  nums[1] = 1
+  nums[(1 * 2) = 2] = nums[1] = 1
+  nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+  nums[(2 * 2) = 4] = nums[2] = 1
+  nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+  nums[(3 * 2) = 6] = nums[3] = 2
+  nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+Hence, nums = [0,1,1,2,1,3,2,3], and the maximum is 3.
+
+Example 2:
+Input: n = 2
+Output: 1
+Explanation: According to the given rules, the maximum between nums[0], nums[1], and nums[2] is 1.
+
+Example 3:
+Input: n = 3
+Output: 2
+Explanation: According to the given rules, the maximum between nums[0], nums[1], nums[2], and nums[3] is 2.
+
+Constraints:
+0 <= n <= 100
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        vector<int> nums(n+1, 0);
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (i == 1)
+                res = nums[i] = 1;
+            if (2*i <= n)
+                nums[2*i] = nums[i];
+            if (2*i+1 <= n)
+                res = max(res, nums[2*i+1] = nums[i]+nums[i+1]);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
