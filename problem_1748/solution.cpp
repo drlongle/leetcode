c150ac66218fde9cddae0172e2dd8447429e1a69
Long Le <drlongle@gmail.com>
2021-02-06 21:25:30 +0100

    Add solution for problem 1748

---
+++ problem_1748/solution.cpp
@@ -0,0 +1,82 @@
+/*
+1748. Sum of Unique Elements
+Easy
+
+You are given an integer array nums. The unique elements of an array are the elements
+that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+Example 1:
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+
+Example 3:
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        for (int n: nums)
+            ++cnt[n];
+        int sum = 0;
+        for (int n: nums) {
+            if (cnt[n] == 1)
+                sum += n;
+        }
+        return sum;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
