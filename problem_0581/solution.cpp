9aa3359ee9b0d56631da2ac2e066926025d29167
Long Le <drlongle@gmail.com>
2021-02-25 19:16:34 +0100

    Add an additional solution for problem 581

--- problem_0581/solution.cpp
+++ problem_0581/solution.cpp
@@ -1,6 +1,6 @@
 /*
 581. Shortest Unsorted Continuous Subarray
-Easy
+Medium
 
 Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
 
