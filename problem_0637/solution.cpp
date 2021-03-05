f13ad436679341554e5c6a8567e382b9b7251a9b
Long Le <drlongle@gmail.com>
2021-03-05 10:07:58 +0100

    Add solution for problem 637

---
+++ problem_0637/solution.cpp
@@ -0,0 +1,58 @@
+/*
+637. Average of Levels in Binary Tree
+Easy
+
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+Note:
+The range of node's value is in the range of 32-bit signed integer.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
