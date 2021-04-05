a083084ed773a4c65985920dcd5e7a5d1eb4d12c
Long Le <drlongle@gmail.com>
2021-04-05 16:53:35 +0200

    Add leetcode solution for problem 32

--- problem_0032/solution.cpp
+++ problem_0032/solution.cpp
@@ -1,7 +1,9 @@
 /*
 32. Longest Valid Parentheses
 
-Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring. For "(()", the longest valid parentheses substring is "()", which has length = 2. Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4. 
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed)
+parentheses substring. For "(()", the longest valid parentheses substring is "()", which has length = 2.
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
 
 See https://www.geeksforgeeks.org/length-of-the-longest-valid-substring/
 */
