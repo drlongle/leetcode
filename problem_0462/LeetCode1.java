0bd8c57d3399a23dad069d0f463298b4e5b4e45b
Long Le <drlongle@gmail.com>
2021-05-19 10:06:18 +0200

    Add additional solution for problem 462

---
+++ problem_0462/LeetCode1.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        long min = Long.MAX_VALUE, sum = 0, total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            long ans = ((long) nums[i] * i - sum) + ((total - sum) - (long) nums[i] * (nums.length - i));
+            System.out.println(nums[i] + " " + ans);
+            min = Math.min(min, ans);
+            sum += nums[i];
+        }
+        return (int) min;
+    }
+}
+
