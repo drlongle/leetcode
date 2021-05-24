05654f41432607576725da03db9f3f82a050e7c8
Long Le <drlongle@gmail.com>
2021-05-24 10:14:07 +0200

    Add solution for problem 709

---
+++ problem_0709/solution.cpp
@@ -0,0 +1,72 @@
+/*
+709. To Lower Case
+Easy
+
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+
+Constraints:
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (char& c: s) {
+            if (isupper(c))
+                c = tolower(c);
+        }
+        return s;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
