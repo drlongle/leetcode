3903c6b27e4c3d3913c7b711e0171beafca408e9
Long Le <drlongle@gmail.com>
2021-08-25 12:28:24 +0200

    Add solution for problem 633

---
+++ problem_0633/solution.cpp
@@ -0,0 +1,93 @@
+/*
+633. Sum of Square Numbers
+Medium
+
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: c = 3
+Output: false
+
+Example 3:
+Input: c = 4
+Output: true
+
+Example 4:
+Input: c = 2
+Output: true
+
+Example 5:
+Input: c = 1
+Output: true
+
+Constraints:
+0 <= c <= 2^31 - 1
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/Node.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+#define for_loop(n) for(int i = 0; i < n; ++i)
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        int limit = sqrt(c);
+        for (long long i = 0; i <= limit; ++i) {
+            int j = c - i*i;
+            int k = sqrt(j);
+            if (k*k == j)
+                return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
