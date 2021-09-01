d67df37f33bd26608e5fd2fc66968aa04fc35933
Long Le <drlongle@gmail.com>
2021-09-01 14:59:21 +0200

    Add solution for problem 565

---
+++ problem_0565/solution.cpp
@@ -0,0 +1,106 @@
+/*
+565. Array Nesting
+Medium
+
+You are given an integer array nums of length n where nums is a permutation of the numbers in the range [0, n - 1].
+
+You should build a set s[k] = {nums[k], nums[nums[k]], nums[nums[nums[k]]], ... } subjected to the following rule:
+
+The first element in s[k] starts with the selection of the element nums[k] of index = k.
+The next element in s[k] should be nums[nums[k]], and then nums[nums[nums[k]]], and so on.
+We stop adding right before a duplicate element occurs in s[k].
+Return the longest length of a set s[k].
+
+Example 1:
+Input: nums = [5,4,0,3,1,6,2]
+Output: 4
+Explanation:
+nums[0] = 5, nums[1] = 4, nums[2] = 0, nums[3] = 3, nums[4] = 1, nums[5] = 6, nums[6] = 2.
+One of the longest sets s[k]:
+s[0] = {nums[0], nums[5], nums[6], nums[2]} = {5, 6, 2, 0}
+
+Example 2:
+Input: nums = [0,1,2]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 10^5
+0 <= nums[i] < nums.length
+All the values of nums are unique.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/Node.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+#define for_loop(n) for(int i = 0; i < n; ++i)
+
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int n = nums.size(), res = 0;
+        vector<int> visited(n, 0), seen;
+        for (int n: nums) {
+            int cnt = 0;
+            while (visited[n] == 0) {
+                visited[n] = 1;
+                n = nums[n];
+                ++cnt;
+            }
+            res = max(res, cnt);
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    vi nums;
+
+    // Output: 4
+    nums = {5,4,0,3,1,6,2};
+    cout << sol.arrayNesting(nums) << endl;
+
+    // Output: 1
+    nums = {0,1,2};
+    cout << sol.arrayNesting(nums) << endl;
+
+    return 0;
+}
