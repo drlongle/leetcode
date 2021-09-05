0921874c437e2a64c7d16c7947eae9cf2e0a8575
Long Le <drlongle@gmail.com>
2021-09-05 16:53:30 +0200

    Add solution for problem 1474

---
+++ problem_1474/solution.cpp
@@ -0,0 +1,102 @@
+/*
+1474. Delete N Nodes After M Nodes of a Linked List
+Easy
+
+Given the head of a linked list and two integers m and n. Traverse the linked list and remove some nodes in the following way:
+Start with the head as the current node.
+Keep the first m nodes starting with the current node.
+Remove the next n nodes
+Keep repeating steps 2 and 3 until you reach the end of the list.
+Return the head of the modified list after removing the mentioned nodes.
+
+Follow up question: How can you solve this problem by modifying the list in-place?
+
+Example 1:
+Input: head = [1,2,3,4,5,6,7,8,9,10,11,12,13], m = 2, n = 3
+Output: [1,2,6,7,11,12]
+Explanation: Keep the first (m = 2) nodes starting from the head of the linked List  (1 ->2) show in black nodes.
+Delete the next (n = 3) nodes (3 -> 4 -> 5) show in read nodes.
+Continue with the same procedure until reaching the tail of the Linked List.
+Head of linked list after removing nodes is returned.
+
+Example 2:
+Input: head = [1,2,3,4,5,6,7,8,9,10,11], m = 1, n = 3
+Output: [1,5,9]
+Explanation: Head of linked list after removing nodes is returned.
+
+Example 3:
+Input: head = [1,2,3,4,5,6,7,8,9,10,11], m = 3, n = 1
+Output: [1,2,3,5,6,7,9,10,11]
+
+Example 4:
+Input: head = [9,3,7,7,9,10,8,2], m = 1, n = 2
+Output: [9,7,8]
+
+Constraints:
+The given linked list will contain between 1 and 10^4 nodes.
+The value of each node in the linked list will be in the range [1, 10^6].
+1 <= m,n <= 1000
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/Node.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+#define for_loop(n) for(int i = 0; i < n; ++i)
+
+class Solution {
+public:
+    ListNode* deleteNodes(ListNode* head, int m, int n) {
+        auto node = head;
+        for(int i = 1; node && i < m; ++i)
+            node = node->next;
+        for (int i = 0; i < n && node && node->next; ++i)
+            node->next = node->next->next;
+        if (node && node->next)
+            node->next = deleteNodes(node->next, m, n);
+        return head;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
