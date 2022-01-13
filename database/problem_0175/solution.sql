f29530ce9e87a9ebb5e9398c0a9de9d74ea88ec7
Long Le <drlongle@gmail.com>
2022-01-13 09:58:42 +0100

    Add solution to problem 181

--- database/problem_0175/solution.sql
+++ database/problem_0175/solution.sql
@@ -1,3 +1,5 @@
+# 175. Combine Two Tables
+
 SELECT DISTINCT Person.firstName, Person.lastName, Address.city, Address.state
        FROM Person
        LEFT JOIN Address ON Person.personId = Address.personId
