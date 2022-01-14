48312a0ec14d21a71dfc8d7eedc6123fb45fc6d9
Long Le <drlongle@gmail.com>
2022-01-14 10:56:16 +0100

    Add additional solution for problem 595

---
+++ database/problem_0595/leetcode.sql
@@ -0,0 +1,17 @@
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area > 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000
+;
+
