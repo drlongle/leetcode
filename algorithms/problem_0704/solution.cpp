27645a6ee64f80f9d4f85e99866b0ec89b761d78
Long Le <drlongle@gmail.com>
2022-03-27 15:34:38 +0200

    Fix bug in solution to problem 704

--- algorithms/problem_0704/solution.cpp
+++ algorithms/problem_0704/solution.cpp
@@ -52,7 +52,7 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size();
+        int low = 0, high = nums.size() - 1;
         while (low <= high) {
             int mid = (low+high)/2;
             int diff = nums[mid] - target;
