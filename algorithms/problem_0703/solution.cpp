01d5cabf978d7a3f339e1077a9d5a806ef4b140a
Long Le <drlongle@gmail.com>
2022-04-08 17:43:26 +0200

    Add solution for problem 703

---
+++ algorithms/problem_0703/solution.cpp
@@ -0,0 +1,103 @@
+/*
+703. Kth Largest Element in a Stream
+Easy
+
+Design a class to find the kth largest element in a stream. Note that it is the
+kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the
+stream of integers nums. int add(int val) Appends the integer val to the stream
+and returns the element representing the kth largest element in the stream.
+
+Example 1:
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+
+Constraints:
+1 <= k <= 10^4
+0 <= nums.length <= 10^4
+-10^4 <= nums[i] <= 10^4
+-10^4 <= val <= 10^4
+At most 10^4 calls will be made to add.
+It is guaranteed that there will be at least k elements in the array when you
+search for the kth element.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "common/ListNode.h"
+#include "common/Node.h"
+#include "common/TreeNode.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+class KthLargest {
+public:
+  KthLargest(int k, vector<int> &nums) : maxsize(k) {
+    for (int n : nums) {
+      pq.push(n);
+    }
+  }
+
+  priority_queue<int, vector<int>, greater<int>> pq;
+  size_t maxsize;
+
+  int add(int val) {
+    pq.push(val);
+    while (pq.size() > maxsize)
+      pq.pop();
+    return pq.top();
+  }
+};
+
+int main() {
+  Solution sol;
+
+  return 0;
+}
