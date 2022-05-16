5928212146fcae666653d66e148e93ac98915402
Long Le <drlongle@gmail.com>
2022-05-16 10:10:55 +0200

    Cosmetic change for solution to problem 2259

--- algorithms/problem_2259/solution.cpp
+++ algorithms/problem_2259/solution.cpp
@@ -9,20 +9,19 @@ Return the resulting string after removing exactly one occurrence of digit from
 number such that the value of the resulting string in decimal form is maximized.
 The test cases are generated such that digit occurs at least once in number.
 
-
-
 Example 1:
-
 Input: number = "123", digit = "3"
 Output: "12"
 Explanation: There is only one '3' in "123". After removing '3', the result is
-"12". Example 2:
+"12".
 
+Example 2:
 Input: number = "1231", digit = "1"
 Output: "231"
 Explanation: We can remove the first '1' to get "231" or remove the second '1'
-to get "123". Since 231 > 123, we return "231". Example 3:
+to get "123". Since 231 > 123, we return "231".
 
+Example 3:
 Input: number = "551", digit = "5"
 Output: "51"
 Explanation: We can remove either the first or second '5' from "551".
