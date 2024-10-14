12d7b7af9335345047ed8a6098680a1418b15348
Long Le <drlongle@gmail.com>
2024-10-14 10:15:24 +0200

    Add solution to problem 2530

---
+++ algorithms/problem_2530/solution.cpp
@@ -0,0 +1,99 @@
+/*
+2530. Maximal Score After Applying K Operations
+Medium
+
+You are given a 0-indexed integer array nums and an integer k. You have a starting score of 0.
+
+In one operation:
+choose an index i such that 0 <= i < nums.length,
+increase your score by nums[i], and
+replace nums[i] with ceil(nums[i] / 3).
+Return the maximum possible score you can attain after applying exactly k operations.
+
+The ceiling function ceil(val) is the least integer greater than or equal to val.
+
+Example 1:
+Input: nums = [10,10,10,10,10], k = 5
+Output: 50
+Explanation: Apply the operation to each array element exactly once. The final score is 10 + 10 + 10 + 10 + 10 = 50.
+
+Example 2:
+Input: nums = [1,10,3,3,3], k = 3
+Output: 17
+Explanation: You can do the following operations:
+Operation 1: Select i = 1, so nums becomes [1,4,3,3,3]. Your score increases by 10.
+Operation 2: Select i = 1, so nums becomes [1,2,3,3,3]. Your score increases by 4.
+Operation 3: Select i = 2, so nums becomes [1,1,1,3,3]. Your score increases by 3.
+The final score is 10 + 4 + 3 = 17.
+ 
+Constraints:
+1 <= nums.length, k <= 10^5
+1 <= nums[i] <= 10^9
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "../common/FenwickTree.h"
+#include "../common/ListNode.h"
+#include "../common/Node.h"
+#include "../common/SegmentTree.h"
+#include "../common/TreeNode.h"
+#include "../common/UnionFind.h"
+#include "../common/bits.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq(begin(nums), end(nums));
+        long long score{0};
+        for (int i = 0; i < k; ++i) {
+            int n = pq.top();
+            pq.pop();
+            score += n;
+            pq.emplace(ceil((double) n / 3));
+        }
+
+        return score;
+    }
+};
+
+int main() {
+    return 0;
+}
