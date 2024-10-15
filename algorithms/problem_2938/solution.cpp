e9e7ce6e875a11ac179c0f397668fb1764dc0c8f
Long Le <drlongle@gmail.com>
2024-10-15 10:51:20 +0200

    Add solution to problem 2938

---
+++ algorithms/problem_2938/solution.cpp
@@ -0,0 +1,101 @@
+/*
+2938. Separate Black and White Balls
+Medium
+
+There are n balls on a table, each ball has a color black or white.
+
+You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white balls, respectively.
+
+In each step, you can choose two adjacent balls and swap them.
+
+Return the minimum number of steps to group all the black balls to the right and all the white balls to the left. 
+
+Example 1:
+Input: s = "101"
+Output: 1
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "011".
+Initially, 1s are not grouped together, requiring at least 1 step to group them to the right.
+
+Example 2:
+Input: s = "100"
+Output: 2
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "010".
+- Swap s[1] and s[2], s = "001".
+It can be proven that the minimum number of steps needed is 2.
+
+Example 3:
+Input: s = "0111"
+Output: 0
+Explanation: All the black balls are already grouped to the right.
+
+Constraints:
+1 <= n == s.length <= 10^5
+s[i] is either '0' or '1'.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "../common/FenwickTree.h"
+#include "../common/ListNode.h"
+#include "../common/Node.h"
+#include "../common/SegmentTree.h"
+#include "../common/TreeNode.h"
+#include "../common/UnionFind.h"
+#include "../common/bits.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long res{0};
+        for (long long sz = s.size(), i = sz - 1, curr = i; i >= 0; --i) {
+            if (s[i] == '1') {
+                res += curr - i;
+                --curr;
+            }
+        } 
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
