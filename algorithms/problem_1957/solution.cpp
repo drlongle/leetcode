c630ffdf1e7574f29f47c32dd69deede6e3b5f6c
Long Le <drlongle@gmail.com>
2024-11-01 09:14:07 +0100

    Add solution to problem 1957

---
+++ algorithms/problem_1957/solution.cpp
@@ -0,0 +1,96 @@
+/*
+1957. Delete Characters to Make Fancy String
+Easy
+
+A fancy string is a string where no three consecutive characters are equal.
+Given a string s, delete the minimum possible number of characters from s to
+make it fancy. Return the final string after the deletion. It can be shown that
+the answer will always be unique.
+
+Example 1:
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+
+Example 2:
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+
+Example 3:
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+
+Constraints:
+1 <= s.length <= 10^5
+s consists only of lowercase English letters.
+*/
+
+#include <algorithm>
+#include <atomic>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string_view>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "../common/FenwickTree.h"
+#include "../common/ListNode.h"
+#include "../common/Node.h"
+#include "../common/SegmentTree.h"
+#include "../common/TreeNode.h"
+#include "../common/UnionFind.h"
+#include "../common/bits.h"
+
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+
+class Solution {
+  public:
+    string makeFancyString(string s) {
+        string res;
+        for (char c : s) {
+            int sz = res.size();
+            if (sz >= 2 && res[sz - 1] == c && res[sz - 2] == c)
+                continue;
+            res.push_back(c);
+        }
+        return res;
+    }
+};
+
+int main() { return 0; }
